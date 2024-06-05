e083893e535a5305ea48ace7574a353f3b7ad6ee
Kevin <XiongL.QiXing@gmail.com>
2024-06-05 13:58:23 -0400

    feat: added page on js & html event page

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -1,2 +1,25 @@
 //Javascript Events
 
+/* HTML Events
+
+An html event can be something the browser does or something a user does
+JS lets you execute code when events are detected
+
+<element event='some JavaScript/function call'>
+
+Common HTML Events
+    onchange - html element is changed
+    onclick - user clicks an html element
+    onmouseover - user moves the mouse over an HTML element
+    onmouseout - user moves the mouse away from an HTML element
+    onkeydown - user pushes a keyboard key
+    onload - browser finishs loading the page
+*/
+
+//JavaScript Event Handlers
+
+/* JS Event Handlers
+
+Event handlers are used to hand and verify user input, user actions, and browser actions
+There are many different methods can be used to let JS work with events.
+*/
